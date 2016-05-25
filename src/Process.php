ef9898d89c6a0ccab4b1fbb9d51ee6b8e031e9b3
Matt Light <matt.light@lightdatasys.com>
2016-05-24 23:40:38 -0700

    Random experimenting

--- src/Process.php
+++ src/Process.php
@@ -2,21 +2,37 @@
 
 namespace Lstr\Commanding;
 
+use Lstr\Sprintf\Middleware\Cli\Bundle;
+use Lstr\Sprintf\Sprintf;
 use Symfony\Component\Process\Process as SymfonyProcess;
 
 class Process
 {
     /**
-     * @var string
+     * @var callable
      */
-    private $command_string;
+    private $command_wrapper;
 
     /**
-     * @param string $command_string
+     * @var array
      */
-    public function __construct($command_string)
+    private $commands;
+
+    /**
+     * @var array
+     */
+    private $starting_command;
+
+    /**
+     * @param callable $command_wrapper
+     * @param array $commands
+     * @param string $starting_command
+     */
+    public function __construct(callable $command_wrapper, array $commands, $starting_command = null)
     {
-        $this->command_string = $command_string;
+        $this->command_wrapper = $command_wrapper;
+        $this->commands = $commands;
+        $this->starting_command = $starting_command;
     }
 
     /**
@@ -41,9 +57,9 @@ class Process
             }
         }
 
-        $last_command_ran = json_decode($non_blank_line, true);
+        $last_command_info = json_decode($non_blank_line, true);
 
-        return $last_command_ran;
+        return $last_command_info['command'];
     }
 
     /**
@@ -55,8 +71,35 @@ class Process
             return $this->process;
         }
 
-        $this->process = new SymfonyProcess($this->command_string);
+        $this->process = new SymfonyProcess(
+            $this->generateCommandString()
+        );
 
         return $this->process;
+    }
+
+    /**
+     * @param string $starting_command
+     * @return string
+     */
+    private function buildScriptString()
+    {
+        $started = false;
+        if (null === $this->starting_command) {
+            $started = true;
+        }
+
+        $selected_commands = [];
+        foreach ($this->commands as $key => $command) {
+            if (!$started && "{$key}" !== "{$this->starting_command}") {
+                continue;
+            }
+
+            $selected_commands[$key] = $command;
+            $started = true;
+        }
+
+        $sprintf = new Sprintf(new Bundle());
+        return $sprintf->sprintf(implode("\n", $selected_commands), $this->params);
     }
 }
--- src/ProcessBuilder.php
+++ src/ProcessBuilder.php
@@ -4,7 +4,6 @@ namespace Lstr\Commanding;
 
 use Lstr\Sprintf\Middleware\Cli\Bundle;
 use Lstr\Sprintf\Sprintf;
-use Symfony\Component\Process\Process;
 
 class ProcessBuilder
 {
@@ -22,26 +21,25 @@ class ProcessBuilder
      * @param array $commands
      * @param array $params
      */
-    public function __construct(array $commands, array $params = [])
+    public function __construct(array $commands)
     {
         $this->commands = $commands;
-        $this->params = $params;
     }
 
     /**
      * @param string $starting_command
      * @return Process
      */
-    public function buildShellProcess($starting_command = null)
+    public function buildShellProcess(array $params, $starting_command = null)
     {
         $script_string = $this->buildScriptString($starting_command);
 
-        $ssh_command = "exec bash -s << 'BASH'\n"
+        $shell_command = "exec bash -s << 'BASH'\n"
             . $script_string
             . "\nexit"
             . "\nBASH";
 
-        return new Process($ssh_command);
+        return new Process($shell_command);
     }
 
     /**
@@ -49,10 +47,8 @@ class ProcessBuilder
      * @param string $starting_command
      * @return Process
      */
-    public function buildSshProcess($ssh_host, $starting_command = null)
+    public function buildSshProcess($ssh_host, array $params, $starting_command = null)
     {
-        $script_string = $this->buildScriptString($starting_command);
-
         $ssh_command = 'exec ssh -q -t -t '
             . escapeshellarg($ssh_host)
             . " 'bash -s' << 'BASH'\n"
@@ -61,30 +57,5 @@ class ProcessBuilder
             . "\nBASH";
 
         return new Process($ssh_command);
-    }
-
-    /**
-     * @param string $starting_command
-     * @return string
-     */
-    private function buildScriptString($starting_command)
-    {
-        $started = false;
-        if (!$starting_command) {
-            $started = true;
-        }
-
-        $selected_commands = [];
-        foreach ($this->commands as $key => $command) {
-            if (!$started && "{$key}" !== "{$starting_command}") {
-                continue;
-            }
-
-            $selected_commands[$key] = $command;
-            $started = true;
-        }
-
-        $sprintf = new Sprintf(new Bundle());
-        return $sprintf->sprintf(implode("\n", $selected_commands), $this->params);
     }
 }
